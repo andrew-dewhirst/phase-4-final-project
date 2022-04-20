cdd24624eb2c052e9613f2ef70dd0acc89ba7fc1
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2022-04-20 16:38:52 -0400

    Minor fixes to controller syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "📃 Seeding data..."
+
+User.create(
+  first_name:"Andrew",
+  last_name:"Dewhirst",
+  email:"andrew@aol.com",
+  username:"andrew.dewhirst",
+  password_digest:"123"
+  )
+
+puts "✅ Done seeding"
