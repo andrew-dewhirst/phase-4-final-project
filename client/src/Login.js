2d640fbb570ec44b11a6173d5ac66a9a85cad59f
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2022-05-01 14:46:31 -0400

    Started CSS file

--- client/src/Login.js
+++ client/src/Login.js
@@ -6,6 +6,8 @@ function Login({ onLogin }) {
 
   return (
     <div>
+      <h2>Welcome to Renov8!</h2>
+      <h4>Share and view renovations from around the world!</h4>
       <SignupForm onLogin={onLogin} />
       <LoginForm onLogin={onLogin} />
     </div>
