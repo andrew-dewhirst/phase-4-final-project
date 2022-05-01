73f4e73085a1b3e002ac02912ed5fce93ec779f4
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2022-05-01 15:16:13 -0400

    Updated CSS for login screen and forms

--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -22,30 +22,33 @@ function LoginForm({ onLogin }) {
       .then(onLogin);
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="username">Username:</label>
-      <input
-        type="text"
-        id="username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <label htmlFor="password">Password:</label>
-      <input
-        type="password"
-        id="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <label htmlFor="password_confirmation">Confirm Password:</label>
-      <input
-        type="password"
-        id="passwordConfirmation"
-        value={passwordConfirmation}
-        onChange={(e) => setPasswordConfirmation(e.target.value)}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <div id="loginform">
+      <h3>Login</h3>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label htmlFor="password_confirmation">Confirm Password:</label>
+        <input
+          type="password"
+          id="passwordConfirmation"
+          value={passwordConfirmation}
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 };
 
--- client/src/SignupForm.js
+++ client/src/SignupForm.js
@@ -29,51 +29,54 @@ function SignupForm({ onLogin }) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="first_name">First Name:</label>
-      <input
-        type="text"
-        id="first_name"
-        value={first_name}
-        onChange={(e) => setFirstName(e.target.value)}
-      />
-      <label htmlFor="last_name">Last Name:</label>
-      <input
-        type="text"
-        id="last_name"
-        value={last_name}
-        onChange={(e) => setLastName(e.target.value)}
-      />
-      <label htmlFor="email">Email:</label>
-      <input
-        type="text"
-        id="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <label htmlFor="username">Username:</label>
-      <input
-        type="text"
-        id="signUpUsername"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <label htmlFor="password">Password:</label>
-      <input
-        type="password"
-        id="signUpPassword"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <label htmlFor="password_confirmation">Confirm Password:</label>
-      <input
-        type="password"
-        id="signUpPasswordConfirmation"
-        value={passwordConfirmation}
-        onChange={(e) => setPasswordConfirmation(e.target.value)}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <div id="signupform">
+      <h3>Sign Up</h3>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="first_name">First Name:</label>
+        <input
+          type="text"
+          id="first_name"
+          value={first_name}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <label htmlFor="last_name">Last Name:</label>
+        <input
+          type="text"
+          id="last_name"
+          value={last_name}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="signUpUsername"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="signUpPassword"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label htmlFor="password_confirmation">Confirm Password:</label>
+        <input
+          type="password"
+          id="signUpPasswordConfirmation"
+          value={passwordConfirmation}
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
 
