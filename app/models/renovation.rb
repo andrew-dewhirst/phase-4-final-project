5e29c3c595486ccbf1c28493ae557735c44209a0
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2022-04-30 21:09:59 -0400

    Added a Likes column to Renovations table

--- app/models/renovation.rb
+++ app/models/renovation.rb
@@ -1,4 +1,4 @@
 class Renovation < ApplicationRecord
   belongs_to :user
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :email, :username, :password, :password_confirmation, presence: true
+  validates :email, uniqueness: true
+
   has_secure_password
   
-  has_many :renovations
-  has_many :reviews
+  has_many :renovations, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 end
